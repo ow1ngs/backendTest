e3265f5ea4be1d42361da3138f7084977090c3a8
ow1ngs <jd.sg1@hotmail.com>
2022-07-30 19:26:49 -0500

    feature: delete sql statements and update constats for new services

--- src/functions/constants.ts
+++ src/functions/constants.ts
@@ -1,11 +1,16 @@
 export const validationMessages = {
-    emailAndPasswordAreValid: "User created succesfully.",
-    emailNotValid: "Incoming email do not match the standars. Must be something@something.com",
-    passwordNotValid:
-      "Incoming password do not match the standars. Password must contain: at least 10 characters, one lowercase letter, one uppercase letter and one of the following characters: !, @, #, ? or ]",
-    emailAlreadyExists: "The incoming email already exists.",
-    ErrorInDB: "Error trying to connect to DB",
-    userCredentialsNotVerified: 'Email or password are not valid.',
-    queryParams: 'Some query params are missing in the request.',
-    CreatedSuccesfully: 'The song was created successfully.'
-}
+  emailAndPasswordAreValid: "User created succesfully.",
+  emailNotValid:
+    "Incoming email do not match the standars. Must be something@something.com",
+  passwordNotValid:
+    "Incoming password do not match the standars. Password must contain: at least 10 characters, one lowercase letter, one uppercase letter and one of the following characters: !, @, #, ? or ]",
+  emailAlreadyExists: "The incoming email already exists.",
+  ErrorInDB: "Error trying to connect to DB",
+  userCredentialsNotVerified: "Email or password are not valid.",
+  queryParams: "Some query params are missing in the request.",
+  CreatedSuccesfully: "The song was created successfully.",
+  UpdatedSuccesfully: "The song was updated successfully.",
+  SongDontExists: "The name of the songs that you are looking for do not exists or the song that you are trying to modify was not created by you.",
+  AuthorizationHeader: "No authorization header is presented in the request.",
+  SongAlreadyExist: "This song name already exists in your collection."
+};
--- src/functions/sqlStatements.ts
+++
@@ -1,3 +0,0 @@
-export const selectUser = `
-  SELECT * from users_table WHERE email=:email
-`;
